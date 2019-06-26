aba3db7f9ba58cc58e3937ad7e99fa51c7cda677
Christian Chiarulli <christian_chiarulli@comcast.com>
2019-06-26 09:47:31 -0400

    added stuff

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { check, validationResult } = require('express-validator/check')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/User')
 
@@ -48,7 +50,23 @@ router.post(
 
       await user.save()
 
-      res.send('User saved')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send('server error')
